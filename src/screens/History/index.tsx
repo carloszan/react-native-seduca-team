5de46fcf9366e4e54c2efa9758bbb486afdb4e46
Carlos Zansavio <undefined751@gmail.com>
2021-05-12 22:15:58 -0300

    historico estilizado

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Container, Text} from './styles';
+import {Container, TopText} from './styles';
 
 export default () => {
   return (
     <Container>
-      <Text>History</Text>
+      <TopText>Historico</TopText>
     </Container>
   );
 };
--- src/screens/History/styles.ts
+++ src/screens/History/styles.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
 
-export const Text = styled.Text``;
+export const TopText = styled.Text`
+  font-weight: bold;
+  font-size: 16px;
+  margin: 30px;
+`;
