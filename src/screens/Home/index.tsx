3004064fa9d63c3fb1eab065da77d6a68b3101c3
Carlos Zansavio <undefined751@gmail.com>
2021-05-12 21:59:13 -0300

    adicionado feed, history e profile screens

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import {Container, Text} from './styles';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Feed from '../Feed';
+import History from '../History';
+import Profile from '../Profile';
+
+const Tab = createBottomTabNavigator();
 
 export default () => {
   return (
-    <Container>
-      <Text>Home</Text>
-    </Container>
+    <Tab.Navigator initialRouteName="History">
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="History" component={History} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
   );
 };
