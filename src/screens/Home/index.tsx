506fdc8eb7397969b787af41ca90e615c92f60ae
Carlos Zansavio <undefined751@gmail.com>
2021-05-12 22:11:28 -0300

    adicionado icones

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,15 +3,40 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Feed from '../Feed';
 import History from '../History';
 import Profile from '../Profile';
+import {MaterialIcons} from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export default () => {
   return (
     <Tab.Navigator initialRouteName="History">
-      <Tab.Screen name="Feed" component={Feed} />
-      <Tab.Screen name="History" component={History} />
-      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <MaterialIcons name="rss-feed" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="History"
+        component={History}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <MaterialIcons name="history" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <MaterialIcons name="face" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
